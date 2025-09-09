68723416bd15aa78fc94c1d23063f761244afcd5
A-V-A12 <a-v-a.12l@protonmail.com>
2025-09-09 23:53:21 +0300

    axios tweak

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-
-const BASE_URL = import.meta.env.MODE === "development" 
-  ? "http://localhost:5001/api" 
-   : import.meta.env.VITE_BACKEND_URL + "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : `${import.meta.env.VITE_BACKEND_URL.replace(/\/$/, "")}/api`;
 
 const api = axios.create({
   baseURL: BASE_URL,
